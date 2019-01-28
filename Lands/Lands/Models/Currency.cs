4ac22cc9920cfa95c35f8c3f568810de24409992
ray-calvo <46980899+ray-calvo@users.noreply.github.com>
2019-01-28 12:58:16 -0600

    List View Personalizada

--- Lands/Lands/Models/Currency.cs
+++ Lands/Lands/Models/Currency.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lands.Models
+﻿namespace Lands.Models
 {
-    class Currency
+    using Newtonsoft.Json;
+    public class Currency
     {
+        [JsonProperty (PropertyName = "code")]
+        public string code { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+
     }
 }
--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,6 +1,126 @@
 ﻿namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using Services;
+    using Xamarin.Forms;
+
+    public class LandsViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiservice;
+        #endregion
+        #region Attributes
+        private ObservableCollection<Land> lands;
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landsList; 
+        #endregion
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands; }
+            set { SetValue(ref this.lands, value); }
+        }
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        public string Filter
+        {
+            get { return this.filter; }
+            set
+            {
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
+        }
+        #endregion
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.apiservice = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+        #region Methods
+        private async void LoadLands()
+        {
+            this.IsRefreshing = true;
+            var connection = await this.apiservice.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    connection.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+
+            }
+            {
+
+            }
+            var response = await this.apiservice.GetList<Land>(
+                "http://restcountries.eu",
+                "/rest",
+                "/v2/all");
+            if (!response.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+            this.landsList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
+            this.IsRefreshing = false;
+        }
+        #endregion
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+            
+        }
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+
+        }
+
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(this.landsList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList.Where(
+                        l => l.Name.ToLower().Contains(this.filter.ToLower()) ||
+                             l.Capital.ToLower().Contains(this.filter.ToLower())));
+                
+            }
+        }
+        #endregion
+
     }
 }
